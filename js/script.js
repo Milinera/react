e42a388f16059b866fd23fd77bddafa70e2a763e
milinera <milineragrov@gmail.com>
2026-01-23 13:26:15 +0300

    передвижение по dom

--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
+//передвижение по DOM
+// console.log(document.body.childNodes) //хранилище внутри блока body, lastchild firstchild
+// const box = document.querySelector('.box'),
+//       block = document.querySelector('.col1'),
+//       item = document.querySelector('[data-item="3"]');   //получание по data-"что-то свое"
+// // console.log(item.nextSibling); //получение следующего node
+// // console.log(item.nextElementSibling);
+// // console.log(box.firstElementChild); //первый ребенок
+// for (let node of box.childNodes) {
+//     if (node.nodeName == '#text') {
+//         continue;
+//     } else {
+//         console.log(node)
+//     }
+// }
+
+
+
+
+
 //создание элементов
-const div = document.createElement('div'),
-      box = document.querySelector('.box'),
-      cols = document.querySelectorAll('.col');
+// const div = document.createElement('div'),
+//       box = document.querySelector('.box'),
+//       cols = document.querySelectorAll('.col');
 
 
-div.classList.add('blac');
+// div.classList.add('blac');
 
-box.append(div); //в конец блока
-box.prepend(div); // в начало
-cols[2].remove(); //удаляем третий элемент
-cols[3].before(div); //вставляет после или after для перед элементом
-cols[0].replaceWith(cols[1]); //заменить cols0 на cols1
+// box.append(div); //в конец блока
+// box.prepend(div); // в начало
+// cols[2].remove(); //удаляем третий элемент
+// cols[3].before(div); //вставляет после или after для перед элементом
+// cols[0].replaceWith(cols[1]); //заменить cols0 на cols1
 
-div.innerHTML = '<h2>hello</h2>';  //добавляем код в блок
-cols[3].textContent = '<h3>hello</h3>'; //только текст для безопасности от пользобвателей 
+// div.innerHTML = '<h2>hello</h2>';  //добавляем код в блок
+// cols[3].textContent = '<h3>hello</h3>'; //только текст для безопасности от пользобвателей 
 
-div.insertAdjacentHTML('afterbegin', '<p>ddd</p>'); //вставить что-то перед, после, до или в элемент
+// div.insertAdjacentHTML('afterbegin', '<p>ddd</p>'); //вставить что-то перед, после, до или в элемент
 
 
 
